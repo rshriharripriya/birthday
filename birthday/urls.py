3353136d87f2a8b938879c6f3bb94e838e4b951f
rshriharripriya <rshriharripriya19@gmail.com>
2020-10-13 00:00:48 +0530

    Initial commit

---
+++ birthday/urls.py
@@ -0,0 +1,39 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path,include
+import os
+from django.contrib import admin
+from django.urls import include, path
+from django.conf.urls import url
+from django.views.generic import TemplateView
+from django.views.static import serve
+import os
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls import url
+from django.contrib.auth import views as auth_views
+from django.views.static import serve
+# Up two folders to serve "site" content
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+SITE_ROOT = os.path.join(BASE_DIR, 'site')
+
+urlpatterns = [
+path('',include('home.urls')),
+    path('admin/', admin.site.urls),
+]
+
---
+++ home/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+from django.views.generic import TemplateView
+app_name='home'
+urlpatterns = [
+    path('', TemplateView.as_view(template_name='home/main.html')),
+
+
+    ]
